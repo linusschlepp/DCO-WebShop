125a6511f7ca960e4a2b1f3ffa393aa88fb3e614
linusschlepp <linusmo.94@googlemail.com>
2023-12-08 13:09:11 +0100

    Implemented payment process

--- src/main/scala/de/othr/dco/webshop/actors/BasketActor.scala
+++ src/main/scala/de/othr/dco/webshop/actors/BasketActor.scala
@@ -12,30 +12,23 @@ object BasketActor {
 
   case class AddItemToUserBasket(user: User, item: Item) extends BasketMessage
 
-  case class InitOrder(user: User) extends BasketMessage
 
-  case class GetAllItemsForUser() extends BasketMessage
+  case class GetAllItemsForUser(replyTo: ActorRef[BasketMessage]) extends BasketMessage
 
-  case class ConductPayment(user: User, payment: Payment, paymentRef: ActorRef[PaymentMessage]) extends BasketMessage
-
-  // ToDO: Maybe response will be need in the future?
-  case class Response(user: User)
+  case class Response(itemList: List[Item]) extends BasketMessage
 
   def apply(): Behavior[BasketMessage] = internalBehavior(List())
 
   def internalBehavior(itemList: List[Item]): Behaviors.Receive[BasketMessage] = {
     Behaviors.receiveMessage {
-      case GetAllItemsForUser() =>
-        // ToDo: Either handle payment action in the BasketActor or return list of items to WebShopActor
-        itemList.foreach(println)
+      case GetAllItemsForUser(basketRef) =>
+        println(s"Returning the following items: $itemList")
+        basketRef ! Response(itemList)
         Behaviors.same
       case AddItemToUserBasket(user, item) =>
         val newList = itemList :+ item
         println(s"Successfully added $item to the basket of user $user, the order now contains: ${newList}")
         internalBehavior(newList)
-      case ConductPayment(user, payment, newPaymentActor) =>
-        // TODO: Handle payment action here?
-        Behaviors.same
     }
   }
 }
--- src/main/scala/de/othr/dco/webshop/entities/Item.scala
+++ src/main/scala/de/othr/dco/webshop/entities/Item.scala
@@ -2,6 +2,5 @@ package de.othr.dco.webshop.entities
 
 class Item(val name: String, val price: Double) {
 
-
   override def toString = s"Item($name, $price)"
 }
